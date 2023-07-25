598e7c3152346f5f73304eeae246259f5711a218
Andrew <kolbe1129@gmail.com>
2023-07-25 17:09:38 -0500

    added more customers

--- database.py
+++ database.py
@@ -4,10 +4,21 @@ conn = sqlite3.connect('customer.db')
 
 c = conn.cursor()
 
-c.execute(""" 
-INSERT INTO customers
-VALUES ('Tim', 'Smith', 'timmy@aol.com')
+customers = [('Bob', 'Jungles', 'bobby@aol.com'), ('Sepp', 'Kuss', 'seppyboy@aol.com'), ('Thibout', 'Pinot', 'thibout@aol.com')]
+
+# c.executemany(""" 
+# INSERT INTO customers
+# VALUES (?, ?, ?)
+# """, customers)
+
+c.execute("""
+SELECT rowid, * FROM customers
 """)
+
+items = c.fetchall()
+
+for item in items:
+    print(item)
 
 conn.commit()
 
