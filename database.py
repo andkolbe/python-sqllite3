5858a15649364482c55ec7bf5e491aee076c5d8c
Andrew <kolbe1129@gmail.com>
2023-07-25 16:40:12 -0500

    added customers to table

--- database.py
+++ database.py
@@ -5,11 +5,8 @@ conn = sqlite3.connect('customer.db')
 c = conn.cursor()
 
 c.execute(""" 
-CREATE TABLE customers(
-    first_name text, 
-    last_name text,
-    email text          
-)
+INSERT INTO customers
+VALUES ('Tim', 'Smith', 'timmy@aol.com')
 """)
 
 conn.commit()
